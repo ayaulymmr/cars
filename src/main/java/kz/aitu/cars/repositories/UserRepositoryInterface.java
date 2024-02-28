ff3ba45af5ae504be3fec787d4f2870723dd5aa0
ayaulymmr <125123051+ayaulymmr@users.noreply.github.com>
2024-02-29 01:20:43 +0600

    Delete src directory

--- src/main/java/kz/aitu/cars/repositories/UserRepositoryInterface.java
+++
@@ -1,7 +0,0 @@
-package kz.aitu.cars.repositories;
-
-import kz.aitu.cars.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepositoryInterface extends JpaRepository<User, Long> {
-}
--- src/main/resources/schema.sql
+++
@@ -1,2 +0,0 @@
-CREATE SCHEMA IF NOT EXISTS cars_schema;
-SET search_path = cars_schema; -- Switch to the newly created schema
